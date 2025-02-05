afb9ff2cd72e581e61d1741412b24bc3a7a67a40
Cormac Geraghty <G00409920@atu.ie>
2025-02-05 15:07:08 +0000

    Created lab2

--- lab1/Perceptron.java
+++ lab1/Perceptron.java
@@ -1,30 +1,28 @@
 public class Perceptron {
     private float[] weights;
     private float threshold = 0.2f;
-    private float alpha = 0.1f;
+    private float learningRate = 0.1f;
 
-    // Initialize perceptron with random weights
-    public Perceptron(int connection) {
-        this.weights = new float[connection];
-
-        for (int i = 0; i < connection; i++) {
-            weights[i] = (float) (Math.random() * 2 - 1);
+    // Initialize perceptron with random positive weights
+    public Perceptron(int inputs) {
+        this.weights = new float[inputs];
+        for (int i = 0; i < inputs; i++) {
+            weights[i] = (float) (Math.random()); // Initialize weights between 0 and 1
         }
         System.out.println("Initial weights: " + weights[0] + ", " + weights[1]);
     }
 
     // Train the perceptron using given data and expected outputs
-    public void train(float[][] data, float[] expected, int max_epochs) {
+    public void train(float[][] data, float[] expected, int epochs) {
         int epoch;
-
-        for (epoch = 0; epoch < max_epochs; epoch++) {
+        for (epoch = 0; epoch < epochs; epoch++) {
             boolean errorFlag = false;
             for (int i = 0; i < data.length; i++) {
                 int output = activate(data[i]);
                 float error = expected[i] - output;
                 if (error != 0) errorFlag = true;
                 for (int j = 0; j < weights.length; j++) {
-                    weights[j] += alpha * error * data[i][j];
+                    weights[j] += learningRate * error * data[i][j];
                 }
             }
             if (!errorFlag) break; // Stop early if no errors
@@ -36,7 +34,6 @@ public class Perceptron {
     // Compute perceptron activation using weighted sum and threshold
     public int activate(float[] inputs) {
         float sum = 0;
-        
         for (int i = 0; i < inputs.length; i++) {
             sum += inputs[i] * weights[i];
         }
